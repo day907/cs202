9c1483a2ad01134e19547d0008a73cc7b26d0961
Jordan Day <vdzday@gmail.com>
2019-09-17 13:56:20 -0800

    Created Box class, variables _height, _width, and _fill, and created associated get / set functions.

--- hw01/box.cpp
+++ hw01/box.cpp
@@ -1,5 +1,22 @@
 #include "box.hpp"
 
-int main() {
-	return 0;
+
+//Getter and Setter functions
+int Box::getHeight() {
+	return _height;
+}
+void Box::setHeight(int height) {
+	_height = height;
+}
+int Box::getWidth() {
+	return _width;
+}
+void Box::setWidth(int width) {
+	_width = width;
+}
+bool Box::getFill() {
+	return _fill;
+}
+void Box::setFill(bool fill) {
+	_fill = fill;
 }
--- hw01/box.hpp
+++ hw01/box.hpp
@@ -1,4 +1,18 @@
 #ifndef BOX_HPP
 #define BOX_HPP
 
+class Box {
+public:
+	int getHeight();
+	void setHeight(int);
+	int getWidth();
+	void setWidth(int);
+	bool getFill();
+	void setFill(bool);
+private:
+	int _height;
+	int _width;
+	bool _fill;
+};
+
 #endif
