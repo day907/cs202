2a88075a1f1daccc1cdcbf6b1be69b1de8c64a8a
Jordan Day <vdzday@gmail.com>
2019-09-19 10:41:49 -0800

    Added include and using statements

---
+++ lab04/lab04.cpp
@@ -0,0 +1,22 @@
+//CS202
+//Jordan Day
+//Lab 4
+//9/19/19
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+#include <sstream>
+using std::istringstream;
+using std::ostringstream;
+#include <string>
+using std::string;
+#include <fstream>
+using std::ofstream;
+using std::ifstream;
+
+int main() {
+
+	return 0;
+}
