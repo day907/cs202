7d0faa82192d879be4f63b8e11db9710dd2f6748
Jordan Day <vdzday@gmail.com>
2019-09-19 11:06:13 -0800

    Added loop to ask user for integer input

--- lab04/lab04.cpp
+++ lab04/lab04.cpp
@@ -2,7 +2,6 @@
 //Jordan Day
 //Lab 4
 //9/19/19
-
 #include <iostream>
 using std::cin;
 using std::cout;
@@ -20,6 +19,7 @@ int main() {
 	string inString;
 	ifstream fileIn("test.txt");
 
+	//1.
 	//Checks if file was found
 	if (!fileIn) {
 		cout << "file did not open" << endl;
@@ -34,5 +34,24 @@ int main() {
 		cout << inString << endl;
 	}
 
+	//2.
+	int nCopies;
+	string copyLine;
+
+	while (true) {
+		//prompt
+		cout << "Input positive integer: ";
+
+		//input retrieval + error checking
+		cin >> nCopies;
+		if (!cin || nCopies < 0) {
+			cin.clear();
+			cin.ignore(999, '\n');
+			cout << "Improper input, try again" << endl;
+			continue;
+		}
+
+
+	}
 	return 0;
 }
