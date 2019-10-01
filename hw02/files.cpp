2c170fe6e1cdcc1f06bee3c457d1f0bbdd95c01b
Jordan Day <vdzday@gmail.com>
2019-09-30 22:11:09 -0800

    Created function getIdFromFile and main in files.cpp

---
+++ hw02/files.cpp
@@ -0,0 +1,15 @@
+#include <string>
+using std::string;
+#include <iostream>
+using std::istream;
+using std::ostream;
+
+int getIdFromFile(const string & fileName,
+	const istream & instream, ostream & outstream) {
+	
+	return 0;
+}
+
+int main() {
+	return 0;
+}
