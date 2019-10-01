b7213ded8d100edafe11eb8d264638a6454a0c99
Jordan Day <vdzday@gmail.com>
2019-09-30 22:17:11 -0800

    created getIdFromFile prototype in files.hpp

--- hw02/files.cpp
+++ hw02/files.cpp
@@ -3,6 +3,7 @@ using std::string;
 #include <iostream>
 using std::istream;
 using std::ostream;
+#include <fstream>
 
 int getIdFromFile(const string & fileName,
 	const istream & instream, ostream & outstream) {
--- hw02/files.hpp
+++ hw02/files.hpp
@@ -1,1 +1,12 @@
 #pragma once
+#ifndef FILES_HPP
+#define FILES_HPP
+#include <string>
+using std::string;
+#include <iostream>
+using std::istream;
+using std::ostream;
+
+int getIdFromFile(const string&, const istream&, ostream&);
+
+#endif
