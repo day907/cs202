08a57eb2a159d297c7cf6814412eb325c39d3880
Jordan Day <vdzday@gmail.com>
2019-09-30 22:42:58 -0800

    Added file opening in getIdFromFile(), small testing features

--- hw02/files.cpp
+++ hw02/files.cpp
@@ -4,13 +4,28 @@ using std::string;
 using std::istream;
 using std::ostream;
 #include <fstream>
+using std::ifstream;
+using std::cout;
+using std::cin;
 
 int getIdFromFile(const string & fileName,
 	const istream & instream, ostream & outstream) {
 	
+	ifstream theFile;
+	theFile.open(fileName);
+
+	if (theFile) {
+		outstream << "File opened";
+	}
+	else if (!theFile) {
+		outstream << "File did not open";
+	}
+
+	theFile.close();
 	return 0;
 }
 
 int main() {
+	getIdFromFile("test.txt", cin, cout);
 	return 0;
 }
