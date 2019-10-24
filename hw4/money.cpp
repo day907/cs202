e3c918c37dd2f41bc1aa78f8b33c275e6485aa43
Jordan Day <vdzday@gmail.com>
2019-10-24 15:31:21 -0800

    Added comments

--- hw4/money.cpp
+++ hw4/money.cpp
@@ -1,3 +1,10 @@
+//CS202
+//Jordan Day
+//10/24/19
+//Homework 4
+//"money.cpp" contains definitions for the Money class
+
+
 #include "money.hpp"
 #include <iostream>
 using std::ostream;
@@ -10,9 +17,15 @@ using std::fixed;
 //Money constructors
 //Default constructor sets _value to zero
 Money::Money() : _value(0) {};
+
+//Rounds the cents parameter to a two digit number, casts it to a double, and
+//adds it to the dollars.
+//e.g. 10, 157 would assign 10.16
 Money::Money(const int dollars, const int cents) :
 	_value(dollars + ((double)roundCents(cents))/100)
 {};
+
+//Rounds the double to nearest hundredths and initializes it to _value
 Money::Money(const double dvalue) :
 	_value(roundCents(dvalue))
 {};
@@ -24,6 +37,9 @@ Money::Money(const double dvalue) :
 //Rounds the cents integer used in two int argument constructor
 int Money::roundCents(int cents) {
 
+	//Procedurally rounds up each digit until it reaches a 2 digit number
+	//could cause a problem with something like cents = 10489 rounding to
+	//11 cents instead of 10.
 	while (abs(cents) >= 100) {
 		if (cents % 10 >= 5) {
 			cents = (cents / 10) + 1;
@@ -37,10 +53,14 @@ int Money::roundCents(int cents) {
 	}
 	return cents;
 }
+
 //Rounds double argument constructor to nearest hundredths
 //All the int casting seemed like the easiest way to truncate?
 double Money::roundCents(double dvalue) {
+	//Extracts thousandths place, maintains sign bit
 	int roundVal = ((int)(dvalue * 1000)) % 10;
+
+	//Checks if the cents round, and if they round positively or negatively
 	if (roundVal >= 5) {
 		dvalue = ((int)(dvalue * 100)) / 100.0 + 0.01;
 	}
@@ -88,41 +108,51 @@ bool operator>=(const Money& mon1, const Money& mon2) {
 Money& Money::operator+=(const Money& rhs) {
 	return *this = *this + rhs;
 }
+
 //Subtract Money from an existing Money object (-=)
 	//canonical
 Money& Money::operator-=(const Money& rhs) {
 	return *this = *this - rhs;
 }
+
 //Add two Money objects
 Money operator+(const Money& mon1, const Money& mon2) {
 	Money newMon(mon1.getValue() + mon2.getValue());
 	return newMon;
 }
+
 //Subtract two Money objects
 Money operator-(const Money& mon1, const Money& mon2) {
 	Money newMon(mon1.getValue() - mon2.getValue());
 	return newMon;
 }
+
 //Multiply a double times a Money object
 Money operator*(const double mon1, const Money& mon2) {
 	Money newMon(mon1 * mon2.getValue());
 	return newMon;
 }
+
 //Multiply a Money object times a double(*)
 Money operator*(const Money& mon1, const double mon2) {
+	//Creating a new Money object ensures the quotient rounds
 	Money newMon(mon1.getValue() * mon2);
 	return newMon;
 }
+
 //Multiply a Money object times a double (*=)
 	//canonical
 Money& Money::operator*=(const double rhs) {
 	return *this = *this * rhs;
 }
+
 //Divide a Money object by a double (/)
 Money operator/(const Money& mon1, const double mon2) {
+	//Creating a new Money object ensures the quotient rounds
 	Money newMon(mon1.getValue() / mon2);
 	return newMon;
 }
+
 //Divide a Money object by a double (/=)
 	//canonical
 Money& Money::operator/=(const double rhs) {
--- hw4/money.hpp
+++ hw4/money.hpp
@@ -1,10 +1,18 @@
+//CS202
+//Jordan Day
+//10/24/19
+//Homework 4
+//"money.hpp" contains prototypes for the Money class
+
 #ifndef MONEY_HPP
 #define MONEY_HPP
 #include <iostream>
 
 class Money {
+//all other comparison operators rely on these two
 friend bool operator==(const Money&, const Money&);
 friend bool operator<(const Money&, const Money&);
+
 public:
 	Money();
 	Money(const int, const int);
@@ -20,6 +28,7 @@ private:
 	int roundCents(int);
 	double _value;
 };
+
 std::ostream& operator<<(std::ostream&, const Money&);
 bool operator!=(const Money&, const Money&);
 bool operator<=(const Money&, const Money&);
@@ -30,4 +39,5 @@ Money operator-(const Money&, const Money&);
 Money operator*(const double, const Money&);
 Money operator*(const Money&, const double);
 Money operator/(const Money&, const double);
+
 #endif
