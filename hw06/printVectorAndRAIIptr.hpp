59e14b0ceb54ecf5f57d09518a650c43aeb45648
Jordan Day <vdzday@gmail.com>
2019-12-02 19:15:55 -0900

    Added class definition for class RAIIPtr

--- hw06/printVectorAndRAIIptr.hpp
+++ hw06/printVectorAndRAIIptr.hpp
@@ -10,6 +10,16 @@ template <typename T>
 void printVector(std::ostream& os, const std::vector<T> vec);
 
 
+template <typename T>
+class RAIIPtr {
+	RAIIPtr(T*);
+	~RAIIPtr();
+	RAIIPtr(const RAIIPtr&) = delete;
+	T operator*(RAIIPtr&);
+	T* operator->();
+};
+
+
 //Definitions
 template <typename T>
 void printVector(std::ostream& os, const std::vector<T> vec) {
